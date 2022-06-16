151a9e11d19f6e34b9c8252791e88d9086989691
Stauffenberg <sriram.aeropsn@gmail.com>
2022-06-16 23:54:58 +0200

    Script updated

--- Callers/generate_cp_ct_map.m
+++ Callers/generate_cp_ct_map.m
@@ -1,82 +1,91 @@
-% Script for generating Cp and Ct map
+% Script for generating Cp (and Ct) map
 
 % Caller script
-clearvars;clc;
+clearvars;close all;clc;
+
+ae = 'G:\BEM\BEM\Data\NREL5MWRefTurb_v50\data\NREL_5MW_ae.txt'; % ae data file
+htc = 'G:\BEM\BEM\Data\NREL5MWRefTurb_v50\htc\NREL_5MW_reference_wind_turbine.htc'; % htc file
+pc = 'G:\BEM\BEM\Data\NREL5MWRefTurb_v50\data\NREL_5MW_pc.txt'; % Aerofoil data file
+
+[BLD.r, BLD.C, BLD.t_C, nsec] = read_ae_file(ae);
+[BLD.AeroTwist] = read_aero_twist(htc, nsec);
+[BLD.pro_t_C, BLD.pro_AoA, BLD.pro_cL, BLD.pro_cD] = read_pc_file(pc);
+BLD.preflap = zeros(length(BLD.r),1);
+
 General.N=3;
 General.rho = 1.225;
-
-ae = readmatrix('G:\BEM\BEM\Data\NREL5MWRefTurb_v50\data\NREL_5MW_ae.txt');
-pc = 'G:\BEM\BEM\Data\NREL5MWRefTurb_v50\data\NREL_5MW_pc.txt';
-
-sec_r = ae(2:size(ae,1),1);
-sec_C = ae(2:size(ae,1),3);
-sec_t_C = ae(2:size(ae,1),5);
-sec_twist = [-13.3,-13.3,-13.3,-13.3,-11.48,-10.16,-9.011,-7.795,-6.544,-5.361,-4.188,-3.125,-2.319,-1.526,-0.863,-0.37,-0.106, 0]'; % to be read from *.htc file
-
-BLD = organize_blade_data(sec_r, sec_C, sec_t_C, sec_twist, pc);
-
 General.induction = 1; % 0 or 1, 0 = Induction Off, 1 = Induction On
 General.tip_loss = 1; % 0 or 1, 0 = Prandtl's tip loss correction Off, 1 = Prandtl's tip loss correction On
 General.highCT = 2; % 0 or 1 or 2, 0 = Off, 1 = As per HANSEN eqn. 6.38, 2 = As per HANSEN eqn. 6.37
 
-lambda = 5:0.5:10;
-op_pts.pitch = (-5:0.5:14)';
+lambda = 2:1:20; % Lambda (TSR) range
+op_pts.pitch = (-5:0.5:25)'; % Pitch range
+wsp = 8; % Just a temp variable for deriving rpm corresponding to lambda
 
-op_pts.wsp = (3:2:18)';
-map_pts.wsp = op_pts.wsp;
-% for i=1:length(op_pts.pitch)
-%     map_pts.pitch(1:length(op_pts.wsp),1) = op_pts.pitch(i);
-%     for j=1:length(lambda)
-%         map_pts.rpm = (lambda(j).*map_pts.wsp/sec_r(end)).*(30/pi);
-%         
-%         [~, output, ~] = core_bem(General, map_pts, BLD);
-%         Cp(i,j) = max(output(:,7)); % The values are anyways same for a Lambda
-%         Ct(i,j) = max(output(:,9));
-%     end
-% end
-
+Cp = zeros(length(op_pts.pitch),length(lambda));
+Ct = zeros(length(op_pts.pitch),length(lambda));
 for i=1:length(op_pts.pitch)
-    map_pts.pitch(1:length(op_pts.wsp),1) = op_pts.pitch(i);
-    for j=1:length(lambda)
-        map_pts.rpm = (lambda(j).*map_pts.wsp/sec_r(end)).*(30/pi);
-        
-        [~, output, ~] = core_bem(General, map_pts, BLD);
-        Cp(i,j) = max(output(:,7)); % The values are anyways same for a Lambda
-        Ct(i,j) = max(output(:,9));
-    end
+    map_pts.wsp(1:length(lambda),1) = wsp;
+    map_pts.pitch(1:length(lambda),1) = op_pts.pitch(i);
+    map_pts.rpm = (lambda'*wsp/BLD.r(end)).*(30/pi);
+    [~, output, ~] = core_bem(General, map_pts, BLD);
+    Cp(i,:) = output(:,7);
+    Ct(i,:) = output(:,9);
 end
-
+%%
+path = 'G:\BEM\BEM\Data\';
+write_cp_ct(lambda,op_pts.pitch,Cp,Ct,path);
 %% Plotting
 
 close all;
-figure()
+MP = get(0,'MonitorPositions');
+if size(MP,1) == 1
+    SS = MP(1,:);
+else
+    SS = MP(2,:); % To have the results better displayed in bigger screen
+end
+figure('position',[SS(1)+SS(3)*0.05, SS(2)+SS(4)*0.2, SS(3)*0.9, SS(4)*0.6])
 
-contourf(op_pts.pitch,lambda,Cp','ShowText','on')
-xlabel('Pitch (deg)');
-ylabel('Lambda (TSR)');
+subplot(1,2,1)
+[C,h] = contourf(lambda',op_pts.pitch,Cp,500,'LineStyle','--');
+hold on
+xlabel('Lambda (TSR)');
+ylabel('Pitch (deg)');
+
+Lvls = h.LevelList;
+h.LevelList = Lvls(Lvls >= 0); 
+h.LevelList = round(h.LevelList,3);
+clabel(C,h);
+
+[x,y] = max_mat(Cp);
+plot(lambda(y),op_pts.pitch(x),'r-p')
+txt = sprintf('  Cp_m_a_x = %0.3f',max(Cp(:)));
+text(lambda(y),op_pts.pitch(x),txt,'Color','red');
+line([lambda(1),lambda(y)],[op_pts.pitch(x),op_pts.pitch(x)],'Color','red','LineStyle','--')
+line([lambda(y),lambda(y)],[op_pts.pitch(1),op_pts.pitch(x)],'Color','red','LineStyle','--')
 hcb = colorbar;
 hcb.Title.String = 'Cp';
 
-%%
-figure()
-for i=1:4:length(op_pts.pitch)
-    plot(lambda,Cp(:,i),'DisplayName',num2str(op_pts.pitch(i)))
-%     ax = plotcontour(lambda,Cp(:,i),op_pts.pitch(i));
-    hold on
-    [max_Cp,idx] = max(Cp(:,i));
-    plot(lambda(idx),max_Cp,'r*','HandleVisibility','off');
+for i=1:length(lambda)
+    [M(i),I(i)] = max(Cp(:,i));
 end
-grid on
-leg=legend('location','nw');
-title(leg,'Pitch (deg)');
-ylim([0 0.593])
-ylabel('Cp')
-xlabel('Lambda (TSR)')
+plot(lambda,op_pts.pitch(I),'.-r','LineWidth',0.5);
 
-%%
-figure()
-plotCpVsLambda(lambda,op_pts.pitch(1:6:39)',(Cp(:,1:6:39)));
+subplot(1,2,2)
+idx_to_plot = unique([flip(x:-4:1) x:4:length(op_pts.pitch)]); % Making sure max Cp is plotted
+j=1;
+for i=[idx_to_plot]
+    [max_Cp(j),idx(j)] = max(Cp(i,:));
+    j=j+1;
+end
+[CM,cc] = plotCpVsLambda(lambda,op_pts.pitch(idx_to_plot)',(Cp(idx_to_plot,:))');
+hold on
+ids_to_plot = reduce(max_Cp);
+plot(lambda(idx(ids_to_plot)),max_Cp(ids_to_plot),'.-r','HandleVisibility','off');
+plot(lambda(y),max(Cp(:)),'r-p')
+text(lambda(y),max(Cp(:)),txt,'Color','red');
 
+%% Supporting functions 
 function [CM,cc] = plotCpVsLambda(inp_x,inp_z,inp_y)
     
     % inp_x: 1xM-vector with x-coordinates
@@ -93,4 +102,69 @@ function [CM,cc] = plotCpVsLambda(inp_x,inp_z,inp_y)
     ylabel('Cp')
     hcb = colorbar;
     hcb.Title.String = 'Pitch (deg)';
+end
+
+function [a,b] = max_mat(c)
+as=size(c);
+total_ele=numel(c);
+[~,I]=max(c(:));
+r=rem(I,as(1));
+a=r;
+b=((I-a)/as(1))+1;
+    if a==0
+        a=as(1);
+        b=b-1;
+    else
+        a=r;
+        b=b;
+    end
+end
+
+function plot_ids = reduce(Cp)
+    Cp_red = Cp;
+    X = Cp(length(Cp));
+    for k = length(Cp)-1:-1:1
+      if Cp(k) < X
+        Cp_red(k) = X;  % Or: NaN
+      else
+        X = Cp(k);
+      end
+    end
+    plot_ids = (Cp_red == Cp);
+end
+
+function write_cp_ct(lambda,pitch,Cp,Ct,path)
+    %% Writing the outputs into a text file
+    outfile = plus(path,"Cp_data.txt");
+    fileID = fopen(outfile,'w');
+    
+    fprintf(fileID,'%d \t %d\n',length(pitch),length(lambda));
+    
+    for i=1:length(pitch)+1
+        if i==1
+            for j=1:length(lambda)+1
+                if j==1
+                    fprintf(fileID,'            \t');
+                else
+                    fprintf(fileID,'%8.3f \t',lambda(j-1));
+                end
+                if j==(length(lambda)+1)
+                    fprintf(fileID,'\n');
+                end
+            end
+        else
+            for j=1:length(lambda)+1
+                if j==1
+                    fprintf(fileID,'%8.3f \t',pitch(i-1));
+                else
+                    fprintf(fileID,'%8.3f \t',Cp(i-1,j-1));
+                end
+                if j==(length(lambda)+1)
+                    fprintf(fileID,'\n');
+                end
+            end
+        end
+        
+    end
+    fclose(fileID);
 end
