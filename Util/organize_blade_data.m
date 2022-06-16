f0cf50dcfc2e0ec4ab6930363ef2b6f4a5b01284
Stauffenberg <sriram.aeropsn@gmail.com>
2022-06-16 23:37:07 +0200

    Renaming variables to be compliant with latest format

--- Util/organize_blade_data.m
+++ Util/organize_blade_data.m
@@ -2,29 +2,26 @@ function Blade = organize_blade_data(sec_r, sec_C, sec_t_C, sec_twist, pc)
     % This function organizes all the blade structural and aerodynamic data
     % in a structure for better data handling
     
-    % sec_r [m] is is the blade section table (m x 1 vector)
-    % sec_C [m] is the Chord corresponding to each of the sec_r (m x 1 vector)
-    % sec_t_C is the thickness to chord ratio corresponding to each of the sec_r (m x 1 vector)
-    % sec_twist [deg] is is the Aerodynamic twist at each of the sec_r (m x 1 vector)
-    % preflap [m] is the section wise offset of each of the sections away from tower (m x 1 vector)
-    % profile_t_C is the K x 1 thickneses available
-    % profile_AoA is the L x 1 angles of attack
-    % profile_cL is L x K cL values corresponding to each angles of attack and t_C
-    % profile_cD is L x K cD values corresponding to each angles of attack and t_C
+    % r [m] is is the blade section table (m x 1 vector)
+    % C [m] is the Chord corresponding to each of the sec_r (m x 1 vector)
+    % t_C is the thickness to chord ratio corresponding to each of the sec_r (m x 1 vector)
+    % AeroTwist [deg] is is the Aerodynamic twist at each of the sec_r (m x 1 vector)
+    % pro_t_C is the K x 1 thickneses available
+    % pro_AoA is the L x 1 angles of attack
+    % pro_cL is L x K cL values corresponding to each angles of attack and t_C
+    % pro_cD is L x K cD values corresponding to each angles of attack and t_C
     
     % Input data organising, Making sure values are in column vector
-    sec_r = sec_r(:);
-    sec_C = sec_C(:);
-    sec_t_C = sec_t_C(:);
-    sec_twist = sec_twist(:);
-    
-    ae = readmatrix('G:\BEM\BEM\Data\NREL5MWRefTurb_v50\data\NREL_5MW_ae.txt');
+    r = sec_r(:);
+    C = sec_C(:);
+    t_C = sec_t_C(:);
+    AeroTwist = sec_twist(:);
     
     % Organizing the blade data in a structure
-    Blade.sec_r = sec_r;
-    Blade.sec_C = sec_C;
-    Blade.sec_t_C = sec_t_C;
-    Blade.sec_twist = sec_twist;
+    Blade.r = r;
+    Blade.C = C;
+    Blade.t_C = t_C;
+    Blade.AeroTwist = AeroTwist;
     Blade.preflap = zeros(length(sec_r),1);
     
     [Blade.pro_t_C, Blade.pro_AoA, Blade.pro_cL, Blade.pro_cD] = read_pc_file(pc);
