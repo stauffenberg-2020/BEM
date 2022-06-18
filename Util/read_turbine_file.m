6b69e6fd2720bfda0a182256d92fb3cf3e37d301
Stauffenberg <sriram.aeropsn@gmail.com>
2022-06-19 01:53:47 +0200

    Turbine file format updated with Control related inputs

--- Util/read_turbine_file.m
+++ Util/read_turbine_file.m
@@ -1,4 +1,4 @@
-function [General, op_pts, Blade] = read_turbine_file(file)
+function [General, op_pts, Blade, Control] = read_turbine_file(file)
 %     coder.inline('never');
     fid = fopen(file,'r');
     
@@ -23,8 +23,9 @@ function [General, op_pts, Blade] = read_turbine_file(file)
         header_rows(i,1) = strncmpi('#',filebyline{i},1);
     end
     header_row_ids = find(header_rows);
-    header_titles = cell(1,4); % Manually defined to have 4 headers, GENERAL, OPERATIONAL_SET-POINTS, BLADE_DETAILS, AEROFOIL_FILE
-    for i=1:4
+    no_h = 5; % Manually defined to have 5 headers, GENERAL, OPERATIONAL_SET-POINTS, BLADE_DETAILS, AEROFOIL_FILE, CONTROL
+    header_titles = cell(1,no_h);
+    for i=1:no_h
         header_titl = filebyline{header_row_ids(i)};
         header_titles{i} = extractAfter(header_titl,'# ');
         header_titles{i} = strtrim(header_titles{i});
@@ -33,6 +34,8 @@ function [General, op_pts, Blade] = read_turbine_file(file)
     % Pre-initilization of structures for MATLAB Coder compatibility
     General = struct('N',0,'rho',0,'induction',0,'tip_loss',0,'highCT',0);
     coder.cstructname(General,'General');
+    Control = struct('Cp_file',0,'Ct_file',0,'Prat',0,'Vin',0,'Vrat',0,'Vout',0,'Oin',0,'Orat',0);
+    coder.cstructname(Control,'Control');
     op_pts = struct('wsp',0,'pitch',0,'rpm',0);
     Blade = struct('r',0,'C',0,'t_C',0,'AeroTwist',0,'preflap',0,'pro_t_C',0,'pro_AoA',0,'pro_cL',0,'pro_cD',0);
     
@@ -75,6 +78,15 @@ function [General, op_pts, Blade] = read_turbine_file(file)
             case 'AEROFOIL_FILE'
                 aerofoil_file = strtrim(filebyline{header_row_ids(i)+1});
                 [Blade.pro_t_C, Blade.pro_AoA, Blade.pro_cL, Blade.pro_cD] = read_pc_file(aerofoil_file);
+            case 'CONTROL'
+                Control.Cp_file = strtrim(extractBefore((filebyline{header_row_ids(i)+1}),';'));
+                Control.Ct_file = strtrim(extractBefore((filebyline{header_row_ids(i)+2}),';'));
+                Control.Prat = real(str2double(extractBefore((filebyline{header_row_ids(i)+3}),';')));
+                Control.Vin = real(str2double(extractBefore((filebyline{header_row_ids(i)+4}),';')));
+                Control.Vrat = real(str2double(extractBefore((filebyline{header_row_ids(i)+5}),';')));
+                Control.Vout = real(str2double(extractBefore((filebyline{header_row_ids(i)+6}),';')));
+                Control.Oin = real(str2double(extractBefore((filebyline{header_row_ids(i)+7}),';')));
+                Control.Orat = real(str2double(extractBefore((filebyline{header_row_ids(i)+8}),';')));
         end
     end
 end
