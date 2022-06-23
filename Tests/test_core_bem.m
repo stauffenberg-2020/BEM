6a73c188c0d9747572d45a52ff0ecd873ab3bb78
Stauffenberg <sriram.aeropsn@gmail.com>
2022-06-23 23:27:37 +0200

    For Coder compatibility

--- Tests/test_core_bem.m
+++ Tests/test_core_bem.m
@@ -1,20 +1,20 @@
-folder = pwd;
 filename = 'NREL_5MW.txt';   
-file = fullfile(folder,filename);
 
-parentDirectory = fileparts(cd);
-addpath(genpath(parentDirectory));
-
-[General, op_pts, Blade, ~] = read_turbine_file(file);
+[General, op_pts, Blade, ~] = read_turbine_file(filename);
 
 Blade.preflap = zeros(length(Blade.r),1);
 
 [output_details, output, BEM] = core_bem(General, op_pts, Blade);
 
-baseline_output = load(fullfile(folder,'baseline_output.mat'));
+try 
+    baseline_output = load('baseline_output.mat');
+catch
+    disp('Unable to load baseline data')
+end
 
 if round(output,4) == round(baseline_output.output,4)
-    disp('Test Passed')
+    disp('Results matching upto 4th decimal, Test Passed')
 else
+    disp('Results not matching')
     throw(exception)
 end
