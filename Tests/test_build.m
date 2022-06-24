3f6e3a0c902bfc7d694a644b9e08c8d9c9d7bd9c
Stauffenberg <sriram.aeropsn@gmail.com>
2022-06-24 22:38:34 +0200

    Auto testing build
    With coder license check included

--- Tests/test_build.m
+++ Tests/test_build.m
@@ -3,23 +3,29 @@ addpath(genpath(pwd))
 cd Compile/
 warning('off','all');
 
-% Build library
-codegen -report -config:lib bem_calc.m -args {'NREL_5MW.txt'}
-disp('Library build successful')
+licen = evalc("license checkout MATLAB_Coder");
+licens = str2double(extractAfter(licen,'='));
 
-% Build MEX
-clear functions
-codegen -report bem_calc.m -args {'NREL_5MW.txt'} -test test_core_bem
-disp('MEX build successful')
+if licens == 1
+    % Build library
+    codegen -report -config:lib bem_calc.m -args {'NREL_5MW.txt'}
+    disp('Library build successful')
 
-% Build EXE
-cfg = coder.config( "exe", "ecoder", false );
-cfg.CustomInclude = "C_Files";
-cfg.CustomSource = "C_Files/main.c";
-cfg.EnableAutoCommit = false;
-cfg.GenerateReport = true;
-cfg.ReportPotentialDifferences = false;
-codegen -config cfg bem_calc.m -args {coder.typeof('NREL_5MW.txt',[1,inf])}
-disp('EXE build successful')
-warning('on','all')
+    % Build MEX
+    clear functions
+    codegen -report bem_calc.m -args {'NREL_5MW.txt'} -test test_core_bem
+    disp('MEX build successful')
 
+    % Build EXE
+    cfg = coder.config( "exe", "ecoder", false );
+    cfg.CustomInclude = "C_Files";
+    cfg.CustomSource = "C_Files/main.c";
+    cfg.EnableAutoCommit = false;
+    cfg.GenerateReport = true;
+    cfg.ReportPotentialDifferences = false;
+    codegen -config cfg bem_calc.m -args {coder.typeof('NREL_5MW.txt',[1,inf])}
+    disp('EXE build successful')
+    warning('on','all')
+else
+    disp('Coder license not available')
+end
