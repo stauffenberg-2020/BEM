df5b69a9dcc79a06f4853a09234e880dd6e4f1de
Stauffenberg <sriram.aeropsn@gmail.com>
2022-07-06 21:57:28 +0200

    Using Turbine file for caller scripts

--- Callers/NREL_HAWC2_caller.m
+++ Callers/NREL_HAWC2_caller.m
@@ -1,25 +1,10 @@
 % Caller script
-clearvars;close all;clc;
+clearvars;clc;
 
-General.N=3;
-General.rho = 1.225;
-General.induction = 1; % 0 or 1, 0 = Induction Off, 1 = Induction On
-General.tip_loss = 1; % 0 or 1, 0 = Prandtl's tip loss correction Off, 1 = Prandtl's tip loss correction On
+filename = 'NREL_5MW.txt';
+[General, op_pts, BLD, ~] = read_turbine_file(filename);
+
 General.highCT = 1; % 0 or 1 or 2, 0 = Off, 1 = As per HANSEN eqn. 6.38, 2 = As per HANSEN eqn. 6.37
-
-op_data = readmatrix('.\Data\NREL5MWRefTurb_v50\data\operational_data.opt','FileType','text');
-ae = '.\Data\NREL5MWRefTurb_v50\data\NREL_5MW_ae.txt'; % ae data file
-htc = '.\Data\NREL5MWRefTurb_v50\htc\NREL_5MW_reference_wind_turbine.htc'; % htc file
-pc = '.\Data\NREL5MWRefTurb_v50\data\NREL_5MW_pc.txt'; % Aerofoil data file
-
-op_pts.wsp = op_data(:,1);
-op_pts.pitch = op_data(:,2);
-op_pts.rpm = op_data(:,3);
-
-[BLD.r, BLD.C, BLD.t_C, nsec] = read_ae_file(ae);
-[BLD.AeroTwist] = read_aero_twist(htc, nsec);
-[BLD.pro_t_C, BLD.pro_AoA, BLD.pro_cL, BLD.pro_cD] = read_pc_file(pc);
-
 BLD.preflap = zeros(length(BLD.r),1);
 
 plot(op_pts.wsp,op_pts.pitch)
@@ -29,6 +14,7 @@ plot(op_pts.wsp,op_pts.pitch)
 
 [output_details, output, BEM] = core_bem(General, op_pts, BLD);
 
+op_data = readmatrix('/Data/NREL5MWRefTurb_v50/data/operational_data.opt','FileType','text');
 
 %% Plotting & Comparisons
 % close all
